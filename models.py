20d184d57321a5c1cb2d29195e686bf048a8d38f
eordiyants <erikray@mail.com>
2024-10-24 18:45:19 +0300

    Добавление класса Поставки деталей

--- models.py
+++ models.py
@@ -11,3 +11,13 @@ class Supplier:
     def __init__(self, supplier_id, name):
         self.supplier_id = supplier_id  # ID поставщика
         self.name = name  # Наименование поставщика
+
+class PartSupply:
+    """
+    'Поставки деталей' для реализации 
+    связи многие-ко-многим
+    """
+    def __init__(self, part_id, supplier_id, quantity):
+        self.part_id = part_id  # ID детали
+        self.supplier_id = supplier_id  # ID поставщика
+        self.quantity = quantity  # Количество поставленной детали
